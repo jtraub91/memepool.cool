6cbe8eae13a2bd9c41074833f95cd0c8b47a2dcb
Jason Traub <jtraub91@gmail.com>
2025-05-09 04:18:00 -0500

    wip

--- app.py
+++ app.py
@@ -30,6 +30,20 @@ def mempool():
         "txids": txids,
     }
 
+@app.route("/tx/<txid>")
+def tx(txid):
+    snapshots = os.listdir("_mempool")
+    sorted_ = sorted(
+        snapshots,
+        key=lambda x: datetime.strptime(x.replace("_", " "), "%Y-%m-%d %H-%M-%S"),
+        reverse=True,
+    )
+    latest = sorted_[0]
+    with open(os.path.join("_mempool", latest, txid), "rb") as f:
+        tx_ = f.read()    
+        
+    return {"txid": txid, "hex": tx_.hex(), "bin": format(int(tx_.hex(), 16), f'0{len(tx_) * 8}b')}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
